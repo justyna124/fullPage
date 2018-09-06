016071e2a890c3117e760220106b5168c9fce6a4
Lenovo <jusnos>
2018-09-06 13:26:50 +0200

    full page

---
+++ app/fullPage.js
@@ -0,0 +1,6 @@
+var checkAnswer = document.getElementById('check-answer');
+
+checkAnswer.addEventListener("click", function () {
+    alert('Najpierw musisz poznać i pokochać siebie, ale to musisz odkryć sam! Miłej podróży w głąb siebie! ;)')
+});
+console.log('beui');
